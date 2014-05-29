815b69a969f592799ff4effdff90d66d202ba426
Jan Cibulka <honza@datastory.cz>
2014-05-29 09:02:53 +0200

    Pridan vypis ulozene sady dat.

--- connector.py
+++ connector.py
@@ -10,5 +10,16 @@ token = proxy.authenticate('anonymous@ipr.praha.eu', '294de3557d9d00b3d2d8a1e6aa
 #vypise schena dostupnych datovych sad v XML
 print(proxy.getschema(token))
 
+#vypise sadu z ulozeneho dotazu c. 21 (automaty), viz. getschema -> storedqueries
+xmlparams = '<?xml version="1.0" encoding="UTF-8"?> \
+<request>\
+<params>\
+</params>\
+</request>'
+
+
+a = proxy.getstoredqueryresult(token, 21, 1000, 0, xmlparams, False)
+print (a)
+
 #zahodi token
 proxy.deauthenticate(token)
