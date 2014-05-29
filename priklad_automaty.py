c2920cc3298a264c0b62230d41d9469f9fbd7449
Jan Cibulka <honza@datastory.cz>
2014-05-29 11:02:44 +0200

    Hotovy priklad exportu preddefinovaneho dotazu - vrstva parkovacich automatu.

---
+++ priklad_automaty.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Python 3
+
+#Priklad pouziti XML-RPC rozhrani IPR Praha. Po prihlaseni stahne a ulozi do csv informace z vrstvy parkovacich automatu.
+
+import xmlrpc.client
+import xml.etree.ElementTree as ET
+import csv
+
+proxy = xmlrpc.client.ServerProxy('http://app.urm.cz/ws/RPC2UTF8/') 
+#ziska token pro uzivatele anonymous@ipr.praha.eu, heslo 'anonymous' (MD5)
+token = proxy.authenticate('anonymous@ipr.praha.eu', '294de3557d9d00b3d2d8a1e6aab028cf')
+
+#vypise schema dostupnych datovych sad v XML
+#print(proxy.getschema(token))
+
+#stahne sadu z ulozeneho dotazu c. 21 (automaty), viz. getschema -> storedqueries a vyexportuje ji do csv
+#souradnicovy system S-JTSK
+xmlparams = '<?xml version="1.0" encoding="UTF-8"?> \
+<request>\
+<params>\
+</params>\
+</request>'
+
+
+response = proxy.getstoredqueryresult(token, 21, 1000, 0, xmlparams, False)
+root = ET.fromstring(response)
+
+with open('./automaty.csv', 'w', newline='') as out:
+    writer = csv.writer(out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['x', 'y', 'hodiny', 'tarif_kc_h', 'tarif_eur', 'typ', 'poznamka'])
+    
+    for automat in root[1]:
+        x = child[0].text
+        y = child[1].text
+        hodiny = child[2].text
+        tarif_kc_h = child[3].text
+        tarif_eur = child[4].text
+        typ = child[5].text
+        poznamka = child[6].text
+    
+        writer.writerow([x, y, hodiny, tarif_kc_h, tarif_eur, typ, poznamka])
+
+#zahodi token
+proxy.deauthenticate(token)
